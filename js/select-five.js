2ed36faba2dfc70b3fd64cc667a8d8738bf74928
amirayhan <jaber.ac1983@gmail.com>
2023-05-28 16:22:20 +0600

    select player and add to player list

--- js/select-five.js
+++ js/select-five.js
@@ -1,8 +1,29 @@
-function elementText(elementId) {
-    const elementTextField = document.getElementById("elementId");
-    const elementText = elementTextField.innerText;
-    return elementText;
+const btnSelect = document.querySelectorAll("#select");
+const selectedPlayer = [];
+for (let btn of btnSelect) {
+    btn.addEventListener("click", function (event) {
+        // get the player name
+        const playerName = event.target.parentNode.children[0].innerText;
+
+        selectedPlayer.push(playerName);
+
+        if (selectedPlayer.length <= 5) {
+            const playerList = document.getElementById("selected_palyer");
+            const li = document.createElement("li");
+            li.innerText = playerName;
+            playerList.appendChild(li);
+            btn.disabled = true;
+            btn.style.backgroundColor = "rgb(59 130 246)";
+        } else {
+            const notification = document.getElementById("notification_content");
+            notification.style.display = "block";
+
+            const warningText = document.getElementById("warning_text");
+            warningText.innerText = "You Can't Select More Than 5 Players";
+
+            document.getElementById("close").addEventListener("click", function () {
+                notification.style.display = "none";
+            });
+        }
+    });
 }
-document.getElementById("select").addEventListener("click", function (event) {
-    const playerTarget = event.target.value;
-});
